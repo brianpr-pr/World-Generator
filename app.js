223a40bbbcff5351fffef03f06ba832f48a260a4
brianpr-pr <brian23perezperez@gmail.com>
2025-11-30 18:52:34 +0000

    Updtate function that draws the 64x64 container

---
+++ app.js
@@ -0,0 +1,37 @@
+// Max number of squares: 3274
+
+document.addEventListener('DOMContentLoaded', function() {
+    const canvas = document.getElementById('canvas');
+    const worldGeneratorForm = document.getElementById('world-generator-form');
+    const button = document.getElementById('generator');
+
+    button.addEventListener('click', function() {
+        generateMap();
+        
+        
+        //validateForm();
+    });
+});
+
+
+
+function generateMap(){
+    for(let i = 1; i <= 4096; i++){
+        let div = document.createElement('div');
+        let classIdentifier = i;
+        div.classList.add('square',classIdentifier);
+        canvas.appendChild(div); 
+    }
+}
+
+
+/*
+const validateForm = () => { 
+    [...world-generator-form.querySelectorAll('div')].forEach(divContainerZone => {
+        validateZoneContainer(divContainerZone);
+    });
+}
+
+function validateZoneContainer(divContainerZone){
+    console.log(divContainerZone);
+}*/
