9fb533c0742b046fdcbc730f915ce19b111f6095
brianpr-pr <brian23perezperez@gmail.com>
2025-12-01 18:04:19 +0000

    Update

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.getElementById('canvas');
     const worldGeneratorForm = document.getElementById('world-generator-form');
     const button = document.getElementById('generator');
+	const form = document.getElementById('world-generator-form');
 
-    button.addEventListener('click', function() {
-        generateMap();
-	paintSquares();
-    });
+	form.addEventListener('submit', function(event){
+		event.preventDefault();
+		generateMap();
+		paintSquares();
+	});
 });
 
 //Work on algorithm to paint squares
@@ -18,30 +20,34 @@ function generateMap(){
 	//squareDiv.classList.add('square');
         canvas.appendChild(squareDiv);
     }
-
 }
 
 function paintSquares(){
 	const startSquare = 2018;
 	let actualSquare = startSquare;
-	let movements = ['top', 'right', 'bottom','left'];
+	let movements = ['top', 'right', 'left', 'bottom'];
 	let numberOfSquaresPainted = 0;
 
 	while(numberOfSquaresPainted  < 3274){
 		//We generate a random move to access the array of movements
-		switch(movements[Math.floor(Math.random() * 4)]){
-		case 'top':
-		  actualSquare -= 64; 
-		  break;
-		case 'right':
-		  actualSquare  += 1;
-		  break;
-		case 'bottom':
-		  actualSquare  += 64;
-		  break;
-		case 'left':
-		  actualSquare  -= 1;
-		  break;
+		
+
+		switch(movements[Math.floor(Math.random() * 7)]){
+			case 'top':
+				actualSquare -= 64;
+				break;
+
+			case 'right':
+				actualSquare  += 1;
+				break;
+
+			case 'left':
+				actualSquare  -= 1;
+				break;
+
+			case 'bottom':
+				actualSquare  += 64;
+				break;
 		}
 
 
@@ -63,13 +69,18 @@ function paintSquares(){
 
 
 
-/*
+
 const validateForm = () => { 
     [...world-generator-form.querySelectorAll('div')].forEach(divContainerZone => {
         validateZoneContainer(divContainerZone);
     });
 }
 
+function validateMapInput(mapSize, mapPorcentageOccupied){
+
+}
+
+/*
 function validateZoneContainer(divContainerZone){
     console.log(divContainerZone);
 }*/
